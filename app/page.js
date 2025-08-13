8278d9df7c16d4221479411e58c2fe2c7ce7dab3
riteshk-007 <codeshorts007@gmail.com>
2025-08-13 21:54:53 +0530

    Comment out BlogPreviewSection in Home component

--- app/page.js
+++ app/page.js
@@ -91,7 +91,7 @@ export default function Home() {
       <FAQSection />
       <TestimonialsSection />
       <ClientLogosSlider />
-      <BlogPreviewSection />
+      {/* <BlogPreviewSection /> */}
     </main>
   );
 }
